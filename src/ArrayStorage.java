cf462fce37835c5bcd47204ed98ca8dca14cd7aa
Evgeny Kalinin <eugene.venzz@gmail.com>
2022-04-13 22:43:39 +0300

    Added logic to methods: save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < firstNullId(storage); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (searchElementId(storage, r.uuid) > -1) {
+            System.out.println("Such element is already exists");
+        } else {
+            storage[firstNullId(storage)] = r;
+        }
     }
 
     Resume get(String uuid) {
+        if (searchElementId(storage, uuid) > -1) {
+            return storage[searchElementId(storage, uuid)];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (searchElementId(storage, uuid) > -1) {
+            for (int i = searchElementId(storage, uuid); i < firstNullId(storage); i++) {
+                storage[i] = storage[i + 1];
+            }
+        } else {
+            System.out.println("Element is out");
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumeStorage = new Resume[firstNullId(storage)];
+        for (int i = 0; i < firstNullId(storage); i++) {
+            resumeStorage[i] = storage[i];
+        }
+        return resumeStorage;
     }
 
     int size() {
-        return 0;
+        return firstNullId(storage);
+    }
+
+    private int firstNullId(Resume[] storage) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length - 1;
+    }
+
+    private int searchElementId(Resume[] storage, String uuid) {
+        for (int i = 0; i < firstNullId(storage); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
